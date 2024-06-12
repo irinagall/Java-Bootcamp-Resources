38b56fc2382512c6af7c220937dc90dccd2d638d
Diana Gall <dianagall47@gmail.com>
2024-06-12 10:33:06 +0100

    Enums workbook code

--- Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Team.java
+++ Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Team.java
@@ -1,10 +1,12 @@
+import constants.Position;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Team {
 
     private String name;
-    private Map<String, String> players;
+    private Map<Position, String> players;
 
     public Team(String name) {
         this.name = name;
@@ -21,13 +23,13 @@ public class Team {
         this.name = name;
     }
 
-    public String getPlayer(String position) {
+    public String getPlayer(Position position) {
         return this.players.get(position);
     }
 
-    public void setPlayer(String position, String player) {
-        if (!position.equals("SHOOTING_GUARD") || !position.equals("SMALL_FORWARD") || !position.equals("POWER_FORWARD") || !position.equals("CENTER") || !position.equals("POINT_GUARD")) {
-            throw new IllegalArgumentException("INVALID POSITION");
+    public void setPlayer(Position position, String player) {
+        if (position ==null) {
+            throw new IllegalArgumentException("POSITION should not be null ");
         }
         this.players.put(position, player);
     }
